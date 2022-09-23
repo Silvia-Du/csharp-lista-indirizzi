8a8368a27b633abd988e1eac9f29785f49f1d065
Silvia-Du <silviadu@hotmail.it>
2022-09-23 17:13:43 +0200

    Implementata verifica dei dati

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Xml.Linq;
+
 Console.WriteLine("Hello, World!");
 /*
      In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto 
@@ -13,12 +16,52 @@ Console.WriteLine("Hello, World!");
 List<Address> addresses = new List<Address>();
 
 //leggo il file 
+//FileStream file = File.Open("./addresses.csv", FileMode.Open);
 
-//istanziare l'oggetto
+StreamReader addressFile = File.OpenText("../../../addresses.csv");
 
-//salvare gli indirizzi nella mia lista
 
-//capire eccezioni possibili
+//tolgo la prima riga
+addressFile.ReadLine();
+//Name,Surname,Street,City,Province,ZIP
+//while (!File.EndOfStream)
+while (!addressFile.EndOfStream)
+{
+    string line = addressFile.ReadLine();
+    string[] rowSplit = line.Split(',');
+    string secondSurname = null;
+    string province;
+
+    int last = rowSplit.Count() - 1;    
+    string[] string2 =  rowSplit[2].Trim().Split(' ');
+
+    // assegno parametri obblig per il costruttore
+    string name = rowSplit[0] == "" ? "-" : rowSplit[0];
+    string surname = rowSplit[1];
+
+    //creo nuova istanza di address
+    Address newAddress = new Address(name , surname);
+
+    //gestione secondo cognome
+    if (rowSplit.Count() > 6 && rowSplit[2].Trim().Count() != 2 && string2.Count() == 1)
+    {
+        secondSurname = rowSplit[2];
+    }
+    newAddress.SecondSurname = secondSurname != null ? secondSurname : "";
+
+
+    //gestione altri paramentri
+    string street = rowSplit[2] == ""? "-": rowSplit[2];
+    string city = rowSplit[3];
+    province = rowSplit[last-1].Trim();
+    string zip = rowSplit[last].Trim().Count() >5? "invalid": rowSplit[last].Trim();
+
+    newAddress.CompleteData(street, city, province, zip);
+
+
+    Console.WriteLine(zip);
+
+}
 
 //generare eccezioni personalizzate
 
@@ -28,20 +71,33 @@ public class Address
 
     string Name { get; }   
     string Surname { get; }
-    string Street { get; }
-    string City { get; }
-    string Province { get; }
-    int ZIP { get; }
+    public string SecondSurname { get; set; }
+    string Street { get; set; }
+    string City { get; set;  }
+    string Province { get; set; }
+    string ZIP { get; set; }
 
-    public Address(string name, string surname, string street, string city, string province, int zip)
+    public Address(string name, string surname)
     {
         Name = name;
         Surname = surname;
-        Street = street;
-        City = city;
-        Province = province;
-        ZIP = zip;
     }
+
+    public void CompleteData(string street, string city,string province, string zip)
+    {
+        try
+        {
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zip;
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
 
 
 }
